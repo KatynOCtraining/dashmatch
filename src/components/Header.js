468091ca6f92504a5ff44ebd8bdda7dc32432a24
KatynOCtraining <t.theveupolaris@gmail.com>
2024-10-06 18:38:04 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css"; // Nous allons ajouter le CSS pour la mise en page
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="./assets/logo.png" alt="Dashmatchs Logo" />
+      </div>
+      <h1 className="site-name">Dashmatchs</h1>
+    </header>
+  );
+}
+
+export default Header;
